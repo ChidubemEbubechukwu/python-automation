f45db43c24eabe7cb7706283d233901279720245
Ebubechukwu Chidubem <ebubechukwuchidubem@outlook.com>
2026-03-24 15:55:04 +0100

    Week 1 Day 2: Completed and extended tasks - Added personal info variables (name, age, favorite color, hobby) - Added task for simple math operations (addition, subtraction, multiplication, division, modulo, exponent) - Added boolean practice (and/or/not logic) - Corrected string-number combination issues - Corrected favourite_number operations (double and half) using int/str/f-strings - Added mini project combining variables, math, and string outputs

--- task.py
+++ task.py
@@ -1,7 +1,52 @@
-food = "Jollof_rice and Chicken"
+name = "Ebubechukwu Chidubem"
+food = "Jollof rice and Chicken"
 colour = "Black" 
 hobby = "Learning new languages and playing football" 
+age = 23
+is_student = True 
+marital_status = "Single" 
+best_friend = "Wisdom Okereke"
+greeting = "Hello"
+message = greeting + " " + name 
+favourite_number = 7
+
+
+print(name)
+print(age)
+print(marital_status)
+print(best_friend)
 print(food)
 print(colour)
 print(hobby)
-print("I love " + food + ", My favourite colour is " + colour + ", and I enjoy " + hobby + " a lot.")
+print(is_student)
+print("My name is "+ name +", I am " + str(age) +" years old, I am " + marital_status + ". I love " + food + ", My favourite number is " + str(favourite_number) + ", as it's a lucky number. My favourite colour is " + colour + ", and I enjoy " + hobby + " a lot.")
+print("Double of my favourite number is: "  + str(favourite_number * 2))
+print("Half of my favourite number is: "  + str(favourite_number / 2))
+print(message)
+print("Bonjour! " * 3)
+
+x = 10
+y = 5
+print("Addition:", x + y)
+print("Subtraction:", x - y)
+print("Multiplication:", x * y)
+print("Division:", x / y)
+print("Modulo:", x % y)
+print("Exponent:", x ** y)
+
+is_happy = True
+is_sad = False
+print(is_happy and is_sad)
+print(is_happy or is_sad)
+print(not is_happy)
+
+print(type(age))
+print(str(age))
+
+num = int("45")
+print(num + 5)
+
+
+
+
+
