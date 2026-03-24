4d35f7b2e8e4e15cc520bd86575043960a538c3d
Ebubechukwu Chidubem <ebubechukwuchidubem@outlook.com>
2026-03-24 13:55:37 +0100

    Week 1 Day 2: Updated hello.py and added task.py for variables exercise

--- hello.py
+++ hello.py
@@ -1,1 +1,9 @@
 print("Hello Github!")
+print("Hello Ebube! Welcome to python automation")
+name = "Ebube"
+age = 23
+is_student = True 
+print(name)
+print(age)
+print(is_student)
+print("My name is " + name + " , I am " + str(age) + " years old.")
---
+++ task.py
@@ -0,0 +1,7 @@
+food = "Jollof_rice and Chicken"
+colour = "Black" 
+hobby = "Learning new languages and playing football" 
+print(food)
+print(colour)
+print(hobby)
+print("I love " + food + ", My favourite colour is " + colour + ", and I enjoy " + hobby + " a lot.")
