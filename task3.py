509597455d33bbf08d5f0496aa09134085717dee
Ebubechukwu Chidubem <ebubechukwuchidubem@outlook.com>
2026-03-25 11:30:23 +0100

    Week 1 Day 3: Added interactive program with user input and calculations

---
+++ task3.py
@@ -0,0 +1,32 @@
+name = input("Enter your name: ")
+age = int(input("Your Age: "))
+
+print("Hello "  + name) 
+print("It's nice to meet you")
+print("You are " , age , "years old")
+print("This means you'd be " , age + 1 , "next year, Happy Birthday in Advance August Girl")
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second numer: "))
+
+print("Addition: ",  num1 + num2)
+print("Subtraction: ", num1 - num2)
+print("Multiplication: ", num1 * num2) 
+print("Division: ", num1 / num2)
+print("Exponent: ", num1 ** num2)
+print(f"The sum of {num1} and {num2} is {num1 + num2}")
+ 
+
+
+print("Class Test")
+
+print("Hello User")
+name = input("Enter your name: ")
+age = int(input("Your age: "))
+fav_num = int(input("Your Favourite number: "))
+print("Hello " + name)
+print("Welcome to Python Programming")
+print("You are ", age , "years old. Next year you will be", age + 1 , ".")
+print(f"Double of your favourite number is {fav_num * 2}. ")
+print(f"While half of your favourite number is {fav_num / 2}")
+print("Thanks for your time")
