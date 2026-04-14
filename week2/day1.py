7a7225ef7711ae2983f971f10dd0cea1a69ee060
Ebubechukwu Chidubem <joelcc460@gmail.com>
2026-04-14 12:33:15 +0100

    Week 2 Day 1 completed - basic input and conditions

---
+++ week2/day1.py
@@ -0,0 +1,13 @@
+# Week 2 Day 1 - Basic Input & Conditions Practice
+
+print("Welcome to Week 2 Day 1")
+
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+if age >= 18:
+    print(name, "you are an adult")
+else:
+    print(name, "you are a minor")
+
+print("End of program")
